77aff111e52522994362295b2a8f7f570c59eb9d
timohafclm <timohafclm@mail.ru>
2017-08-15 15:32:27 +0300

    Update GUI

---
+++ src/com/company/client/ClientGuiController.java
@@ -0,0 +1,64 @@
+package com.company.client;
+
+public class ClientGuiController extends Client {
+    private ClientGuiModel model = new ClientGuiModel();
+    private ClientGuiView view = new ClientGuiView(this);
+
+    public static void main(String[] args) {
+        new ClientGuiController().run();
+    }
+
+    public ClientGuiModel getModel() {
+        return model;
+    }
+
+    @Override
+    public void run() {
+        getSocketThread().run();
+    }
+
+    @Override
+    protected SocketThread getSocketThread() {
+        return new GuiSocketThread();
+    }
+
+    @Override
+    protected String getServerAddress() {
+        return view.getServerAddress();
+    }
+
+    @Override
+    protected int getServerPort() {
+        return view.getServerPort();
+    }
+
+    @Override
+    protected String getUserName() {
+        return view.getUserName();
+    }
+
+    public class GuiSocketThread extends SocketThread {
+        @Override
+        protected void processIncomingMessage(String message) {
+            model.setNewMessage(message);
+            view.refreshMessages();
+        }
+
+        @Override
+        protected void informAboutAddingNewUser(String userName) {
+            model.addUser(userName);
+            view.refreshUsers();
+        }
+
+        @Override
+        protected void informAboutDeletingNewUser(String userName) {
+            model.deleteUser(userName);
+            view.refreshUsers();
+        }
+
+        @Override
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            view.notifyConnectionStatusChanged(clientConnected);
+        }
+    }
+}
